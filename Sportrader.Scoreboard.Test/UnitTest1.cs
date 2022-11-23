34af08ec09b609bea680950214cd38e103390c48
Mohammad R. Farahy <mfarahy@gmail.com>
2022-11-23 12:04:32 +0100

    Add project files.

---
+++ Sportrader.Scoreboard.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Sportrader.Scoreboard.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
